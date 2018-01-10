dca9438a1ef775f8fa976e752430eb63c56f6e99
Narolf Nathan John Dela Rosa <ndelarosa@YouSource.Forest>
2018-01-10 10:56:45 +0800

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Aether' },
+  { id: 12, name: 'Black Lion' },
+  { id: 13, name: 'Clayfist' },
+  { id: 14, name: 'Deleter' },
+  { id: 15, name: 'Emperoar' },
+  { id: 16, name: 'Flaskus' },
+  { id: 17, name: 'Gastrognome' },
+  { id: 18, name: 'Hotwire' },
+  { id: 19, name: 'Inkjet' },
+  { id: 20, name: 'Jade Blade' }
+];
